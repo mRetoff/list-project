b975d96db00929b956fcc4bb5b93cb2143d9f0a4
Matthew Retoff <matt.retoff@yahoo.com>
2018-05-16 20:38:41 -0400

    Add image

--- index.js
+++ index.js
@@ -2,16 +2,24 @@ const form = document.querySelector('#input')
 
 function handleSubmit(ev) {
     ev.preventDefault()
-    const m = ev.movie.value
+    const temp = ev.target
+    const m = temp.movie.value
 
-    renderList()
+    renderList(m)
+
+    if(m.toLowerCase().indexOf('infinity war') >= 0)
+        document.getElementById('img').style.display = 'block'
+
+    temp.reset()
 }
 
 function renderList(data) {
     const movieList = document.querySelector('#list')
     const list = document.createElement('ul')
+    const item = document.createElement('li')
 
-    list.appendChild(data)
+    item.textContent = data
+    list.appendChild(item)
     movieList.appendChild(list)
 }
 
